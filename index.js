209a6039a3f3ab06d17e65b74f104680954e6dbc
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-05-27 00:21:23 +0530

    feat(client): Implement user registration form and handle server response
    feat(client): Implement user registration form and handle server response
    
    * Introduces a registration form with username and password fields.
    * Fetches user registration data upon form submission using `fetch` API.
    * Sends username and password in a JSON body with appropriate headers.
    * Handles the server response and retrieves the generated user ID.
    * Redirects the user to a potential profile page with the user ID.
    * Provides basic user feedback by redirecting to the profile after registration.
    * Establishes a foundation for user interaction and server communication.

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const port = 3000;
 const app = express();
 
 app.use(express.static('./public'));
+app.use(express.json());
+
+// states
+const userStore = {}
+
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+    const id = `user_${Date.now()}`;
+
+    const user = {
+        id,
+        username,
+        password
+    }
+
+    userStore[id] = user;
+
+    console.log(`Registered user with id`, userStore[id]);
+
+    return res.json({ id });
+
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);})
