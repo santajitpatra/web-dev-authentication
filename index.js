b3961ae5d91a44dddb172c2aff986c5f0b502481
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-05-28 00:54:08 +0530

    feat(server, client): Enhance user registration with WebAuthn integration
    feat(server, client): Enhance user registration with WebAuthn integration
    
    * Extends user registration to include WebAuthn challenge generation.
    * Introduces a new server-side route for requesting a registration challenge ("/register-challenge").
    * Updates the client-side registration flow to handle the WebAuthn challenge response.
    * Integrates `@simplewebauthn/server` for generating WebAuthn registration options.
    * Utilizes WebAuthn for a more secure user registration process on the client-side.
    * Provides a more robust user registration experience.

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-const express = require('express');
+const express = require("express");
+const { generateRegistrationOptions } = require("@simplewebauthn/server");
 
 const port = 3000;
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
 // states
-const userStore = {}
+const userStore = {};
+const challengeStore = {};
 
-app.post('/register', (req, res) => {
-    const {username, password} = req.body;
-    const id = `user_${Date.now()}`;
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const id = `user_${Date.now()}`;
 
-    const user = {
-        id,
-        username,
-        password
-    }
+  const user = {
+    id,
+    username,
+    password,
+  };
 
-    userStore[id] = user;
+  userStore[id] = user;
 
-    console.log(`Registered user with id`, userStore[id]);
+  console.log(`Registered user with id`, userStore[id]);
 
-    return res.json({ id });
+  return res.json({ id });
+});
 
-})
+app.post("register-challenge", async (req, res) => {
+  const { userId } = req.body;
+
+  if (!userStore[userId]) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const challengePayload = await generateRegistrationOptions({
+    // id: userId,
+    rpId: "localhost",
+    rpName: "Localhost",
+    username: userStore[userId].username,
+    // password: userStore[userId].password,
+    // authenticatorSelection: {
+    //   authenticatorAttachment: "platform",
+    //   userVerification: "required",
+    // },
+    // timeout: 60000,
+  });
+
+  challengeStore[userId] = challengePayload.challenge;
+
+  return res.json({ option: challengePayload });
+});
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);})
+  console.log(`Server started on port ${port}`);
+});
