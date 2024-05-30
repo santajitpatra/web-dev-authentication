a4169ba41905a04bb3da9273d4f5cfb0fcea6ea0
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-05-31 00:11:28 +0530

    feat(server, client): Complete WebAuthn integration for registration and login
    feat(server, client): Complete WebAuthn integration for registration and login
    
    * Extends WebAuthn support to include login functionalities.
    * Introduces server-side routes for handling login challenges and verifications.
    * Implements client-side interaction for initiating WebAuthn login using a user ID.
    * Integrates `@simplewebauthn/server` for generating login challenges and verifying responses.
    * Validates login attempts against the user's registered passkey information.
    * Provides a comprehensive user experience for secure registration and login with WebAuthn.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const crypto = require("node:crypto");
 const express = require("express");
-const { generateRegistrationOptions, verifyRegistrationResponse } = require("@simplewebauthn/server");
+const { generateRegistrationOptions, verifyRegistrationResponse, generateAuthenticationOptions,verifyAuthenticationResponse } = require("@simplewebauthn/server");
 const { error } = require("node:console");
 
 if (!globalThis.crypto) {
@@ -16,6 +16,7 @@ app.use(express.json());
 // states
 const userStore = {};
 const challengeStore = {};
+const loginChallengeStore = {};
 
 app.post("/register", (req, res) => {
   const { username, password } = req.body;
@@ -76,6 +77,42 @@ app.post("/register-verify", async (req, res) => {
   return res.json({ verified: true });
 })
 
+app.post("/login-challenge", async (req, res) => {
+  const { userId } = req.body;
+  if (!userStore[userId]) return res.status(404).json({ error: "User not found" });
+  
+  const opts = await generateAuthenticationOptions({
+    rpID: "localhost",
+  });
+
+  loginChallengeStore[userId] = opts.challenge;
+
+  return res.json({ options: opts });
+});
+
+app.post("/login-verify", async (req, res) => {
+  const { userId, response } = req.body;
+
+  if (!userStore[userId])
+    return res.status(404).json({ error: "User not found" });
+
+  const user = userStore[userId];
+  const challenge = loginChallengeStore[userId];
+
+  const verifyLoginResult = await verifyAuthenticationResponse({
+    expectedChallenge: challenge,
+    expectedOrigin: "http://localhost:3000",
+    expectedRPID: "localhost",
+    response: response,
+    authenticator: user.passkey,
+  });
+
+  if (!verifyLoginResult.verified) {
+    return res.status(400).json({ error: "Invalid registration response" });
+  }
+
+  return res.json({ success: true, userId }); 
+});
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
